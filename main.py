f8bd24ed70b3cfef7efd328aeed32005a8685898
Tushar Gaurav <tushar.gaurav416@gmail.com>
2021-01-07 09:00:16 +0000

    initial commit

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import discord
 import os
 import requests
 import json
+import random
+from replit import db
+
 
 client = discord.Client()
 
 curse = ["sad", "happy", "pro", "noob", "coding"]
+
+anti_curse = [
+  "Abe tu hai noob!",
+  "Lega kya be?",
+  "Nikal paheli fursat mei!"
+]
 
 def get_quote():
   response = requests.get('https://zenquotes.io/api/random')
@@ -23,11 +32,16 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
+  msg = message.content
+
+  if msg.startswith('$hello'):
     await message.channel.send('Hello!')
-  elif message.content.startswith('$gyan'):
+  elif msg.startswith('$gyan'):
     quote = get_quote()
     await message.channel.send(quote)
+
+  if any(word in msg for word in curse):
+    await message.channel.send(random.choice(anti_curse))
 
 
 client.run(os.getenv('TOKEN'))
