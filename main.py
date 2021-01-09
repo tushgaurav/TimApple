16b31cd89dea14175de4da86568e2a4862016719
Tushar Gaurav <tushar.gaurav416@gmail.com>
2021-01-09 11:16:38 +0530

    added many features

--- main.py
+++ main.py
@@ -4,45 +4,82 @@ import requests
 import json
 import random
 import csv
+from decouple import config
 
+TOKEN = config('TOKEN')
 
 client = discord.Client()
 
 # list of curse words created using csv
-curse = []
+load_words = []
 curse_file = "censor/bad-words-en.csv"
 with open(curse_file, 'r') as word:
     for i in csv.reader(word):
-        curse.append(i)
+        load_words.append(i)
 
-curse_final = []
+curse_en = []
 
-for i in curse:
-    index = curse.index(i)
-    curse_final.append(curse[index][0])
+for i in load_words:
+    index = load_words.index(i)
+    curse_en.append(load_words[index][0])
+
+load_words.clear()
+
+curse_file = "censor/bad-words-hindi.csv"
+with open(curse_file, 'r') as word:
+    for i in csv.reader(word):
+        load_words.append(i)
+
+curse_hi = []
+
+for i in load_words:
+    index = load_words.index(i)
+    curse_hi.append(load_words[index][0])
 
 # messages if curse words are used in the server
-anti_curse = [
+anti_curse_en = [
     "Please mind your language.",
-    "Don't use that language here in this server.",
+    "Don't use that language here in this server!",
     "You could face temporary ban if you keep on using that language!"
+    "Don't use foul language on this server!"
 ]
 
-# fetch quotes from zenquotes.io
+anti_curse_hi = [
+    "मेरा मुंह मत खुलवाओ!",
+    "में आजकल रंग बदलने में लोगों का मुकाबला नहीं कर पा रहा हूं!!",
+    "तारीफों से भी जिंदा रह सकते हैं..",
+    "सूखे हुए पत्तों की तरह मत बनाओ अपनी जिंदगी!"
+]
 
+
+################
+# api
+################
 
 def get_quote():
     response = requests.get('https://zenquotes.io/api/random')
     json_data = json.loads(response.text)
     quote = json_data[0]['q'] + " -" + json_data[0]['a']
-    #quote = json_data[0]['h']
+    # quote = json_data[0]['h']
     return(quote)
+
+
+def evil_insult():
+    response = requests.get(
+        'https://evilinsult.com/generate_insult.php?lang=en&type=json')
+    json_data = json.loads(response.text)
+    insult = json_data['insult']
+    return(insult)
+
+################
+# main bot code
+################
 
 
 @client.event
 async def on_ready():
     print(
-        "We have logged in as {0.user} /n Bot created by Tushar G. (github.com/tushgaurav)".format(client))
+        "Logged in as {0.user} /n Bot created by Tushar G. (github.com/tushgaurav)".format(client))
 
 
 @client.event
@@ -52,14 +89,24 @@ async def on_message(message):
 
     msg = message.content
 
+# bot functions
     if msg.startswith('$hello'):
         await message.channel.send('Hello!')
-    elif msg.startswith('$gyan'):
+
+    if msg.startswith('$gyan'):
         quote = get_quote()
         await message.channel.send(quote)
 
-    if any(word in msg for word in curse_final):
-        await message.channel.send(random.choice(anti_curse))
+    if msg.startswith('$roast'):
+        insult = evil_insult()
+        await message.channel.send(insult)
+
+# anti curse features
+    if any(word in msg for word in curse_en):
+        await message.channel.send(random.choice(anti_curse_en))
+
+    if any(word in msg for word in curse_hi):
+        await message.channel.send(random.choice(anti_curse_hi))
 
 
-client.run('Nzk1Mjc1NzQyMTkxMjg4MzIx.X_HAQA.bYEYoDBVRMt__xiqedIKi3M7rLQ')
+client.run(TOKEN)
